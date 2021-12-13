ee10f248fe1c963a48fce075e61362cfba7005db
Advait Shirvaikar <53732078+advshirk99@users.noreply.github.com>
2021-12-13 15:28:07 +0530

    Update main.go

--- golang-sample/main.go
+++ golang-sample/main.go
@@ -1,36 +1,49 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Sample run-helloworld is a minimal Cloud Run service.
 package main
 
 import (
-    "html/template"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
-type ContactDetails struct {
-    Email   string
-    Subject string
-    Message string
+func main() {
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func main() {
-    tmpl := template.Must(template.ParseFiles("forms.html"))
-
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method != http.MethodPost {
-            tmpl.Execute(w, nil)
-            return
-        }
-
-        details := ContactDetails{
-            Email:   r.FormValue("email"),
-            Subject: r.FormValue("subject"),
-            Message: r.FormValue("message"),
-        }
-
-        // do something with details
-        _ = details
-
-        tmpl.Execute(w, struct{ Success bool }{true})
-    })
-
-    http.ListenAndServe(":8080", nil)
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", name)
 }
